30d16e9cf19cdd1e3a85c9f0e1cb5de407009abc
alephOne1 <ov4.t@yandex.ru>
2018-08-16 18:15:10 +0600

    Add multi assignment error handling

--- main/pyscal.py
+++ main/pyscal.py
@@ -1,5 +1,15 @@
 import sys
 
+class Constants:
+    ASSIGNMENT_OPERATOR = ":="
+    ADDITION_OPERATOR = "+"
+    SUBTRACTION_OPERATOR = "-"
+
+    UNARY = "unary",
+    BINARY = "binary"
+
+    CONTAINER_OBJECT = "CONTAINER"
+
 
 class Operation:
     def __init__(self, operation_string, operation_type):
@@ -8,6 +18,8 @@ class Operation:
 
 
 class Parser:
+    const = Constants()
+
     def __init__(self, code_text, available_operations):
         rows = self.get_code_rows(code_text)
 
@@ -23,42 +35,87 @@ class Parser:
     def get_code_rows(self, any_code):
         return [code_row for code_row in any_code.split("\n") if code_row]
 
-    def get_action_from_row(self, any_code_row):
+    def get_action_from_row(self, any_code_row, level=0):
         for operation_item in self.operations.items():
             if operation_item[1].operator in any_code_row:
-                if operation_item[1].type == "unary":
+                if operation_item[1].type == self.const.UNARY:
                     current_row_parts = [
                         row_part.strip() for row_part in any_code_row.split(
                             operation_item[1].operator
                         )
                     ]
 
-                    if operation_item[1].operator == ":=":
-                        return {operation_item[0]: current_row_parts}
+                    if len(current_row_parts) > 2:
+                        raise SyntaxError("There must be only one assignment in the row")
+
+                    if operation_item[1].operator == self.const.ASSIGNMENT_OPERATOR:
+                        if level > 0:
+                            raise SyntaxError("There must be only one assignment in the row")
+                        else:
+                            return {
+                                operation_item[0]: [
+                                    current_row_parts[0],
+                                    self.get_action_from_row(
+                                        current_row_parts[1],
+                                        level + 1
+                                    )
+                                ]
+                            }
+                else:
+                    current_row_parts = [
+                        row_part.strip() for row_part in any_code_row.split(
+                            operation_item[1].operator
+                        )
+                    ]
+                    if operation_item[1].operator == self.const.ADDITION_OPERATOR:
+                        pass
+
+        return any_code_row
 
 
 class Executor:
+    const = Constants()
+
     def __init__(self, algorithm):
-        self.data = {}
+        self.data = {self.const.CONTAINER_OBJECT: None}
 
         for action in algorithm:
             self.run(action)
 
     def run(self, action):
         action_items = action.items()[0]
-        if action_items[0] == "assign":
-            self.assign(
-                *action_items[1]
-            )
+        
+        getattr(self, action_items[0])(
+            *action_items[1]
+        )
 
     def assign(self, variable_name, value):
         self.data[variable_name] = value
 
+    def add(self, first_object, second_object):
+        self.data[
+            self.const.CONTAINER_OBJECT
+        ] = first_object + second_object
+
+    def subtract(self, first_object, second_object):
+        return first_object - second_object
+
 
 class Pyscal:
-    assignment = Operation(":=", "unary")
-    addition = Operation("+", "binary")
-    subtraction = Operation("-", "binary")
+    const = Constants()
+
+    assignment = Operation(
+        const.ASSIGNMENT_OPERATOR,
+        const.UNARY
+    )
+    addition = Operation(
+        const.ADDITION_OPERATOR,
+        const.BINARY
+    )
+    subtraction = Operation(
+        const.SUBTRACTION_OPERATOR,
+        const.BINARY
+    )
 
     OPERATIONS = {
         "assign": assignment,
