03f5bf936df4f3eed70d642d57afe1c0edcb8842
alephOne1 <ov4.t@yandex.ru>
2018-08-18 21:20:47 +0600

    Added simple subtraction opportunity, fixed bugs with addition, variable -> value in expressions.

--- main/pyscal.py
+++ main/pyscal.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import json
 
 class Constants:
     ASSIGNMENT_OPERATOR = ":="
@@ -14,6 +15,12 @@ class Constants:
     BINARY = "binary"
 
     CONTAINER_OBJECT = "CONTAINER"
+
+    RESERVED_WORDS = (
+        "true",
+        "false",
+        "none"
+    )
 
 
 class Operation:
@@ -114,17 +121,22 @@ class Executor:
         }
 
         for action in algorithm:
-            self.run(action)
+            self.run(action, operation=list(action.keys())[0])
 
-    def run(self, action):
-        action_items = action.items()[0]
+    def run(self, action, operation=0):
+        action_items = list(action.items()[0])
+        action_items[1] = sorted(
+            action_items[1],
+            key=lambda element: type(element) == dict
+        )
 
         for item in action_items[1]:
             if type(item) == dict:
-                self.run(item)
+                self.run(item, list(action.keys())[0])
         
         getattr(self, action_items[0])(
-            *action_items[1]
+            *action_items[1],
+            operation=operation
         )
 
     def process_string_variable(self, variable_value):
@@ -137,36 +149,59 @@ class Executor:
                 return variable_value.replace('"', "")
             elif len(re.findall(r'''\d*\.\d+''', variable_value)) == 1:
                 return float(variable_value)
+            elif len(re.findall(r'''[a-zA-Z_]+[\da-zA-Z_]*''', variable_value)) == 1 and variable_value not in self.const.RESERVED_WORDS:
+                if variable_value in self.data and variable_value != "CONTAINER":
+                    return self.data[variable_value]
             elif variable_value == "true":
                 return True
             elif variable_value == "false":
                 return False
             elif variable_value == "none":
                 return None
+        elif type(variable_value) == dict:
+            return self.data[self.const.CONTAINER_OBJECT]
         else:
             return variable_value
 
-    def assign(self, variable_name, value):
+    def assign(self, variable_name, value, operation):
         if type(value) == dict:
             self.data[variable_name] = self.data[self.const.CONTAINER_OBJECT]
         else:
             self.data[variable_name] = self.process_string_variable(value)
 
-    def add(self, *args):
-        result = self.process_string_variable(args[0])
+        self.data[self.const.CONTAINER_OBJECT] = None
+
+
+    def add(self, *args, **kwargs):
+        if type(args[0]) == dict:
+            result = 0
+        else:
+            result = self.process_string_variable(args[0])
 
         for arg in args[1:]:
-            result += self.process_string_variable(arg)
+            if type(arg) == dict:
+                break
+            else:
+                result += self.process_string_variable(arg)
 
+        if self.data[self.const.CONTAINER_OBJECT] is not None:
+            result += self.data[self.const.CONTAINER_OBJECT]
+        
         self.data[self.const.CONTAINER_OBJECT] = result
 
-    def subtract(self, *args):
+    def subtract(self, *args, **kwargs):
         result = self.process_string_variable(args[0])
 
         for arg in args[1:]:
             result -= self.process_string_variable(arg)
 
-        self.data[self.const.CONTAINER_OBJECT] = result
+        if kwargs["operation"] == "add":
+            if self.data[self.const.CONTAINER_OBJECT] is not None:
+                self.data[self.const.CONTAINER_OBJECT] += result
+            else:
+                self.data[self.const.CONTAINER_OBJECT] = result
+        else:
+            self.data[self.const.CONTAINER_OBJECT] = result
 
 
 class Pyscal:
@@ -205,7 +240,14 @@ class Pyscal:
 
         code_executor = Executor(code_parser.actions)
 
-        print(code_executor.data)
+        
+        print(
+            json.dumps(
+                code_executor.data,
+                ensure_ascii=False,
+                indent=4
+            )
+        )
 
     def read_file(self, any_file_path):
         with open(any_file_path, "r") as code_file:
